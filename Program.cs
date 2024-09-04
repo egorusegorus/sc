958bb9cd9ad3ee5672babd13c7d4ea65148151f3
egorusegorus <tomaszokraszewski@gmail.com>
2024-09-04 07:37:40 +0200

    ki version

--- Program.cs
+++ Program.cs
@@ -1,142 +1,109 @@
 ﻿using System;
 
-class HelloWorld 
+class SubnetCalculator
 {
-    static void Main(string[] args) 
+    static void Main(string[] args)
     {
         Console.Clear();
         Console.BackgroundColor = ConsoleColor.DarkGray;
         Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("Herzlich willkommen in SubnetCalculator");
-        Console.WriteLine("");
+        Console.WriteLine("Herzlich willkommen in SubnetCalculator\n");
         
-        int[] Ip = IpEingabe();
-        int CidrVar = Cidr();
-        int[] SubnetzMaske = SubMaske(CidrVar);
-        int[] NetzerkAdresse = new int[4];
-        int[] BroadCastAdresse = new int[4];
+        int[] ip = IpEingabe();
+        int cidrVar = Cidr();
+        int[] subnetzMaske = SubMaske(cidrVar);
+        int[] netzwerkAdresse = new int[4];
+        int[] broadCastAdresse = new int[4];
         
-        NetzwekAdresseMethode(NetzerkAdresse, Ip, SubnetzMaske, ref BroadCastAdresse);
+        BerechneNetzwerkUndBroadcastAdresse(netzwerkAdresse, ip, subnetzMaske, ref broadCastAdresse);
         
-        Console.WriteLine("Sie haben:            " + string.Join(".", Ip) + "/" + CidrVar + " eingegeben");
-        Console.WriteLine("Subnetzwerkmaske:     " + string.Join(".", SubnetzMaske) + " = " + CidrVar + " CIDR Index");
-        Console.WriteLine("Netzwerkadresse:      " + string.Join(".", NetzerkAdresse));
-        Console.WriteLine("Erste freie Adresse:  " + NetzerkAdresse[0]+"."+ NetzerkAdresse[1]+"."+ NetzerkAdresse[2]+"."+ (NetzerkAdresse[3]+1));
-        Console.WriteLine("Letzte freie Adresse: " + BroadCastAdresse[0]+"."+ BroadCastAdresse[1]+"."+ BroadCastAdresse[2]+"."+ (BroadCastAdresse[3]-1));
-        Console.WriteLine("Broadcastadresse:     " + string.Join(".", BroadCastAdresse));
+        Console.WriteLine($"Sie haben:            {string.Join('.', ip)}/{cidrVar} eingegeben");
+        Console.WriteLine($"Subnetzwerkmaske:     {string.Join('.', subnetzMaske)} = {cidrVar} CIDR Index");
+        Console.WriteLine($"Netzwerkadresse:      {string.Join('.', netzwerkAdresse)}");
+        Console.WriteLine($"Erste freie Adresse:  {string.Join('.', netzwerkAdresse[0], netzwerkAdresse[1], netzwerkAdresse[2], netzwerkAdresse[3] + 1)}");
+        Console.WriteLine($"Letzte freie Adresse: {string.Join('.', broadCastAdresse[0], broadCastAdresse[1], broadCastAdresse[2], broadCastAdresse[3] - 1)}");
+        Console.WriteLine($"Broadcastadresse:     {string.Join('.', broadCastAdresse)}");
     }
 
-
-public static int[] SubMaske(int CidrVar)
-{
-    int[] SubnetzMaske = new int[4];
-    
-    int fullOctets = CidrVar / 8; // Liczba pełnych oktetów (o wartości 255)
-    int remainingBits = CidrVar % 8; // Pozostałe bity w oktetach
-
-    for (int i = 0; i < fullOctets; i++)
+    // Methode zur Berechnung der Subnetzmaske basierend auf dem CIDR-Wert
+    public static int[] SubMaske(int cidrVar)
     {
-        SubnetzMaske[i] = 255;
+        int[] subnetzMaske = new int[4];
+        int volleOktetten = cidrVar / 8; // Anzahl der vollständigen Oktetten (255)
+        int verbleibendeBits = cidrVar % 8; // Verbleibende Bits im nächsten Oktett
+
+        for (int i = 0; i < volleOktetten; i++)
+        {
+            subnetzMaske[i] = 255;
+        }
+
+        if (volleOktetten < 4)
+        {
+            subnetzMaske[volleOktetten] = (byte)(255 << (8 - verbleibendeBits) & 255);
+        }
+
+        return subnetzMaske;
     }
 
-    if (fullOctets < 4)
-    {
-        SubnetzMaske[fullOctets] = (byte)(255 << (8 - remainingBits) & 255);
-    }
-
-    return SubnetzMaske;
-}
-    public static int[] NetzwekAdresseMethode(int[] NetzwerkAdresse, int[] Ip, int[] SubnetzMaske, ref int[] BroadCastAdresse)
+    // Methode zur Berechnung der Netzwerk- und Broadcast-Adresse
+    public static void BerechneNetzwerkUndBroadcastAdresse(int[] netzwerkAdresse, int[] ip, int[] subnetzMaske, ref int[] broadCastAdresse)
     {
         for (int i = 0; i < 4; i++)
         {
-            NetzwerkAdresse[i] = Ip[i] & SubnetzMaske[i];
-            BroadCastAdresse[i] = NetzwerkAdresse[i] | (255 - SubnetzMaske[i]);
+            netzwerkAdresse[i] = ip[i] & subnetzMaske[i];
+            broadCastAdresse[i] = netzwerkAdresse[i] | (255 - subnetzMaske[i]);
         }
-
-        return NetzwerkAdresse;
     }
 
+    // Methode zur Eingabe und Validierung des CIDR-Werts
     public static int Cidr()
     {
-        int Cidr = -1;
-        bool WertIstInOrdnung = false;
+        int cidr = -1;
+        bool wertIstInOrdnung = false;
         
-        while (!WertIstInOrdnung)
+        while (!wertIstInOrdnung)
         {
             Console.WriteLine("Bitte geben Sie den CIDR Index ein (0-32):");
-            string? ip = Console.ReadLine();
+            string? eingabe = Console.ReadLine();
 
-            if (ip != null)
+            if (int.TryParse(eingabe, out int test) && test >= 0 && test <= 32)
             {
-                int test;
-                try
-                {
-                    test = Convert.ToInt32(ip);
-                    if (test >= 0 && test <= 32)
-                    {
-                        Cidr = test;
-                        WertIstInOrdnung = true;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl im Bereich von 0 bis 32 ein.");
-                    }
-                }
-                catch (FormatException)
-                {
-                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine gültige Zahl ein.");
-                }
+                cidr = test;
+                wertIstInOrdnung = true;
             }
             else
             {
-                Console.WriteLine("Eingabe ist ungültig. Bitte geben Sie einen Wert ein.");
+                Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl im Bereich von 0 bis 32 ein.");
             }
         }
 
-        return Cidr;
+        return cidr;
     }
 
+    // Methode zur Eingabe und Validierung der IP-Adresse
     public static int[] IpEingabe()
     {
-        int[] IP = new int[4];
+        int[] ip = new int[4];
 
         for (int i = 0; i < 4; i++)
         {
-            int a = i + 1;
-            bool WertIstInOrdnung = false;
-            while (!WertIstInOrdnung)
+            bool wertIstInOrdnung = false;
+            while (!wertIstInOrdnung)
             {
-                Console.WriteLine("Bitte geben Sie den " + a + "-ten Teil der IP-Adresse ein (0-255):");
-                string? ip = Console.ReadLine();
+                Console.WriteLine($"Bitte geben Sie den {i + 1}. Teil der IP-Adresse ein (0-255):");
+                string? eingabe = Console.ReadLine();
 
-                if (ip != null)
+                if (int.TryParse(eingabe, out int teilIp) && teilIp >= 0 && teilIp <= 255)
                 {
-                    int test;
-                    try
-                    {
-                        test = Convert.ToInt32(ip);
-                        if (test >= 0 && test <= 255)
-                        {
-                            IP[i] = test;
-                            WertIstInOrdnung = true;
-                        }
-                        else
-                        {
-                            Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl im Bereich von 0 bis 255 ein.");
-                        }
-                    }
-                    catch (FormatException)
-                    {
-                        Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine gültige Zahl ein.");
-                    }
+                    ip[i] = teilIp;
+                    wertIstInOrdnung = true;
                 }
                 else
                 {
-                    Console.WriteLine("Eingabe ist ungültig. Bitte geben Sie einen Wert ein.");
+                    Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine Zahl im Bereich von 0 bis 255 ein.");
                 }
             }
         }
-        return IP;
+        return ip;
     }
 }
